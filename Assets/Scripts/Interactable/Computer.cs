3f881fa1628d5b6b0e3c2b98683b7147cff83ca4
MunchDuster <72060614+MunchDuster@users.noreply.github.com>
2021-09-12 17:23:45 +1200

    Added some props to room. (Poster, blackboard, drawers, computer desk and computer (Monitor, Mouse, Keyboard and PC))

---
+++ Assets/Scripts/Interactable/Computer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Computer : Interactable
+{
+    public override InteractionInfo TakeItem(Interactable item)
+	{
+		return InteractionInfo.None;
+	}
+	public override bool CanHoldItem(Interactable item)
+	{
+		return false;
+	}
+	protected override string WouldInteractWithOther(Interactable other)
+	{
+		Debug.Log(typeof(Interactable));
+		//if(other)
+		return null;
+	}
+	protected override string WouldInteractWithPlayer(PlayerInteract player)
+	{
+		return null;
+	}
+
+    protected override InteractionInfo InteractWithOther(Interactable other)
+	{
+		//Nothing
+		return InteractionInfo.None;
+	}
+	protected override InteractionInfo InteractWithPlayer(PlayerInteract player)
+	{
+		//Nothing
+		return InteractionInfo.None;
+	}
+}
