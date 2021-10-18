6bff632ac98e2e5102953122f2c507037ddf4526
MunchDuster <72060614+MunchDuster@users.noreply.github.com>
2021-10-18 17:35:44 +1300

    Started on cation lesson.

--- Assets/Scripts/Chemicals/Chemical.cs
+++ Assets/Scripts/Chemicals/Chemical.cs
@@ -2,37 +2,37 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [CreateAssetMenu(fileName = "Chemical", menuName = "Chemical", order = 1)]
-public class Chemical: ScriptableObject
+public class Chemical : ScriptableObject
 {
-    [System.Serializable]
-    private struct Reaction
-    {
-        public Chemical product;
-        public Chemical reactant;
-    }
-    public Chemical mix(Chemical other)
-    {
-        foreach(Reaction reaction in reactions)
-        {
-            if(reaction.reactant == other)
-            {
-                return reaction.product;
-            }
-        }
-        return null;
-    }
-    
-    [SerializeField]
-    private Reaction[] reactions;
-    
-    public string symbol;
-    public Color contentsColor;
-    
-    public bool hasPrecipitate;
-    
-    public Color precipitateColor;
+	[System.Serializable]
+	private struct Reaction
+	{
+		public Chemical product;
+		public Chemical reactant;
+	}
+	public Chemical mix(Chemical other)
+	{
+		foreach (Reaction reaction in reactions)
+		{
+			if (reaction.reactant == other)
+			{
+				return reaction.product;
+			}
+		}
+		return null;
+	}
+
+	[SerializeField]
+	private Reaction[] reactions;
+
+	public string symbol;
+	public Color contentsColor = Color.white;
+
+	public bool hasPrecipitate;
+
+	public Color precipitateColor = Color.white;
 
 	public bool bubbles;
-    
-    public bool makesLitmusBlue;
+
+	public bool makesLitmusBlue;
 }
